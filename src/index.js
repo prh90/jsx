ef5959b9744345797110ce3ffcb7caab2f231241
Pablo Hernandez <prh90@yahoo.com>
2019-05-13 20:27:35 -0700

    Starting to display react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Start = () => {
+  return (
+    <h1>Hi there!</h1>
+  )
+}
+// class Start extends React.Component {
+//   render() {
+//     return(
+//       <h1>Hello World!</h1>
+//     )
+//   }
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Start/>, document.getElementById('app'));
