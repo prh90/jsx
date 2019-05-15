e64491932a98db60948d8fbf1f4e04c36f749aa1
Pablo Hernandez <prh90@yahoo.com>
2019-05-15 00:32:54 -0700

    Adds input

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 
 const Start = () => {
   return (
-    <h1>Hi there!</h1>
+    <div>
+      <label className="label" htmlFor="name">Enter name:</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: "blue", color: "white"}}>Submit</button>
+    </div>
   )
 }
-// class Start extends React.Component {
-//   render() {
-//     return(
-//       <h1>Hello World!</h1>
-//     )
-//   }
-// };
 
 ReactDOM.render(<Start/>, document.getElementById('app'));
