3c3831ef307a85d76013d145f7096d5cc38a0940
Pablo Hernandez <prh90@yahoo.com>
2019-05-27 21:31:36 -0700

    Adds approval card component and displays it in index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Faker from "faker";
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard/>
       <CommentDetail author={Faker.name.firstName()} timeAgo="Today at 6:00pm" src={Faker.image.avatar()} comment="Good Post!"/>
       <CommentDetail author={Faker.name.firstName()} timeAgo="Yesterday at 3:14pm" src={Faker.image.avatar()} comment="Bombness!"/>
       <CommentDetail author={Faker.name.firstName()} timeAgo="2 days go at 4:04pm" src={Faker.image.avatar()} comment="Yass!"/>
