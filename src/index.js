9c529af9bc987a59f3775f6609fd0dbb7924f22e
Pablo Hernandez <prh90@yahoo.com>
2019-06-03 19:19:58 -0700

    Adds fix

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard/>
-      <CommentDetail author={Faker.name.firstName()} timeAgo="Today at 6:00pm" src={Faker.image.avatar()} comment="Good Post!"/>
-      <CommentDetail author={Faker.name.firstName()} timeAgo="Yesterday at 3:14pm" src={Faker.image.avatar()} comment="Bombness!"/>
-      <CommentDetail author={Faker.name.firstName()} timeAgo="2 days go at 4:04pm" src={Faker.image.avatar()} comment="Yass!"/>
+      <ApprovalCard>
+        <CommentDetail author={Faker.name.firstName()} timeAgo="Today at 6:00pm" src={Faker.image.avatar()} comment="Good Post!"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author={Faker.name.firstName()} timeAgo="Yesterday at 3:14pm" src={Faker.image.avatar()} comment="Bombness!"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author={Faker.name.firstName()} timeAgo="2 days go at 4:04pm" src={Faker.image.avatar()} comment="Yass!"/>
+      </ApprovalCard>
     </div>
   )
 };
